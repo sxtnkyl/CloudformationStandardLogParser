fedc93523660379818c99df44b4c5447e3ca230a
unknown <sxtnkyl@gmail.com>
2021-03-29 16:15:53 -0400

    updated filterfolder.js to sort month, not day

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 ## Goal
 
-For use with low traffic websites. Can be used to locally store logs and keep S3 buckets decluttered. 
-Parse Cloudfront standard logs for valid JSON objects, sort into folders by date, 
-and create two compiled jsons (bots and unique viewers).
+For use with low traffic websites. Can be used to locally store logs and keep S3 buckets decluttered.
+Parse Cloudfront standard logs for valid JSON objects, sort into folders by date,
+and create two compiled jsons (bots and unique viewers) for a month's folder.
 
-*NOTE: compile by bots and unique viewers currently separate*
+_NOTE: compile by bots and unique viewers currently separate_
 
 ### Setup
 
-* AWS create credentials profile
-* Connect to aws toolkit 
+- AWS create credentials profile
+- Connect to aws toolkit
   *https://docs.aws.amazon.com/toolkit-for-vscode/latest/userguide/connect.html*
-* Set global credentials file
-* Set global config file
+- Set global credentials file
+- Set global config file
   *https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/global-config-object.html*
-* Dotenv file should contain KEYID, SECRET, REGION, BUCKET  
+- Dotenv file should contain KEYID, SECRET, REGION, BUCKET
 
 ### Javascript
 
 #### **index.js**
 
-Creates a folder in working directory by date(year, month, day). First arguement should be a number *keyAmount* of files 
-to download from your S3 bucket, defined in dotenv. Starts by oldest file in the bucket. 
+Creates a folder in working directory by date(year, month, day). First arguement should be a number _keyAmount_ of files
+to download from your S3 bucket, defined in dotenv. Starts by oldest file in the bucket.
 
-*NOTE: Bucket must not have files nested in folders*
+_NOTE: Bucket must not have files nested in folders_
 
 ```sh
 node index.js 6
@@ -31,9 +31,9 @@ node index.js 6
 
 #### **filterFolder.js**
 
-Optional usage- takes a folder of one day and organizes logs from bots from unique viewers 
-into two json files (botVisits.json and visitorsByCip.json). Takes three arguements- year, month, and day.
-Year must be 4 chars, while month and day are 2 chars. 
+Optional usage- takes a folder of one month and organizes logs from bots from unique viewers
+into two json files (botVisitsByMonth01.json and visitorsByMonth01.json). Takes two arguements- yearand month.
+Year must be 4 chars, while month is 2 chars.
 
 ```sh
 node filterFolder.js 2021 02 17
--- filterFolder.js
+++ filterFolder.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const p = require("path");
-//code to execute once files are loaded into a folder
-//output goal: create 2 conglomerate JSON for a single day: people by c-ip(visitorsByCip.JSON) and bots by CS user agent(botVisits.JSON)
-//input params: year/month/day
+//best to execute at end of month once all files are loaded into a folder
+//output goal: create 2 conglomerate JSON for a single month (bots and people {day: {cip: [{}, {}...]}})
+//inputs = month and year
 
 function checkArgs() {
   //handle missing args
-  if (process.argv.length < 5) {
+  if (process.argv.length < 4) {
     console.log("missing args");
     process.exit(1);
   }
@@ -19,72 +19,75 @@ function checkArgs() {
     console.log("month format needs exactly 2 chars");
     process.exit(1);
   }
-  if (process.argv[4].length != 2) {
-    console.log("day format needs exactly 2 chars");
-    process.exit(1);
-  }
 }
 checkArgs();
 
 const year = process.argv[2];
 const month = process.argv[3];
-const day = process.argv[4];
-const filepath = p.join(__dirname + "/" + year + "/" + month + "/" + day);
-const peoplePath = `${filepath}/visitorsByCip.json`;
-const botPath = `${filepath}/botVisits.json`;
-const filesNames = [];
-const peopleJSON = {};
-const botsJSON = {};
+const monthPath = p.join(__dirname + "/" + year + "/" + month);
+const peoplePathMonth = `${monthPath}/visitorsByMonth${month}.json`;
+const botPathMonth = `${monthPath}/botVisitsByMonth${month}.json`;
+const allMonthFiles = [];
+const monthBotObj = {};
+const monthPeopleObj = {};
 
-//does folder have files? make list of json filenames
-fs.readdir(filepath, (err, files) => {
-  if (err) console.log(err);
-  if (files.length == 0) {
-    console.log("no files are in this folder");
-    process.exit(1);
-  } else {
-    filterJsonFiles();
-    filterFolder();
-  }
+//get all files for month
+function recursivelyListDir(dir, callback) {
+  fs.readdirSync(dir).forEach((f) => {
+    let dirPath = p.join(dir, f);
+    let isDirectory = fs.statSync(dirPath).isDirectory();
+    isDirectory
+      ? recursivelyListDir(dirPath, callback)
+      : callback(p.join(dir, f));
+  });
+}
 
-  function filterJsonFiles() {
-    files.filter((file) => {
-      if (p.extname(file).toLowerCase() === ".json") {
-        //dont include compiled files
-        if (file == "visitorsByCip.json" || file == "botVisits.json") {
-          return;
-        } else filesNames.push(file);
-      }
-    });
-  }
-  //go through file list, sort each key/value pair into either people or bot obj, write to file
-  function filterFolder() {
-    filesNames.forEach((file) => {
-      const currentFile = `${filepath}/${file}`;
-      filterFile(currentFile);
-    });
+function makeMonthObjects() {
+  allMonthFiles.forEach((file) => {
+    const currentJson = JSON.parse(fs.readFileSync(file));
 
-    fs.writeFileSync(peoplePath, JSON.stringify(peopleJSON));
-    fs.writeFileSync(botPath, JSON.stringify(botsJSON));
-
-    function filterFile(file) {
-      const currentJson = JSON.parse(fs.readFileSync(file));
-      for (let key in currentJson) {
-        //bot detector, has string 'bot' somewhere in cs(User-Agent) key
-        let hasBot = currentJson[key][0]["cs(User-Agent)"]
-          .toLowerCase()
-          .includes("bot");
-        if (hasBot) {
-          !botsJSON.hasOwnProperty(key)
-            ? (botsJSON[key] = currentJson[key])
-            : (botsJSON[key] = botsJSON[key].concat(currentJson[key]));
+    //key is a unique c-ip > is arr of objects
+    for (let key in currentJson) {
+      let date = currentJson[key][0]["date"];
+      let day = date.substr(8, 2);
+      let obj = currentJson[key];
+      //hasbot is string with 'bot' somewhere in cs(User-Agent) key
+      let hasBot = currentJson[key][0]["cs(User-Agent)"]
+        .toLowerCase()
+        .includes("bot");
+      if (hasBot) {
+        if (!monthBotObj.hasOwnProperty(day)) {
+          monthBotObj[day] = {};
+          monthBotObj[day][key] = obj;
+        } else {
+          if (!monthBotObj[day].hasOwnProperty(key)) {
+            monthBotObj[day][key] = [].concat(obj);
+          } else monthBotObj[day][key].concat(obj);
         }
-        if (!hasBot) {
-          !peopleJSON.hasOwnProperty(key)
-            ? (peopleJSON[key] = currentJson[key])
-            : (peopleJSON[key] = peopleJSON[key].concat(currentJson[key]));
+      }
+      if (!hasBot) {
+        if (!monthPeopleObj.hasOwnProperty(day)) {
+          monthPeopleObj[day] = {};
+          monthPeopleObj[day][key] = obj;
+        } else {
+          if (!monthPeopleObj[day].hasOwnProperty(key)) {
+            monthPeopleObj[day][key] = [].concat(obj);
+          } else monthPeopleObj[day][key].concat(obj);
         }
       }
     }
-  }
+  });
+
+  fs.writeFileSync(peoplePathMonth, JSON.stringify(monthPeopleObj));
+  fs.writeFileSync(botPathMonth, JSON.stringify(monthBotObj));
+}
+
+recursivelyListDir(monthPath, function (filePath) {
+  //exclude our compiled files if already made
+  if (
+    filePath.includes("visitors") == false ||
+    filePath.includes("botVisits") == false
+  )
+    allMonthFiles.push(filePath);
 });
+makeMonthObjects();
